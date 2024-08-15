bb42a36a01fc352916264b9e463f2dfc57608148
Arun M <123149917+ArunMuthukumarasamy@users.noreply.github.com>
2024-08-15 11:18:08 +0530

    Add files via upload

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my-rds-db.c18804ywoj0x.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Arun2130',
+    DB_DATABASE : 'webappdb'
+});
